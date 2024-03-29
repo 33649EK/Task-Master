bf5e6ab44965bd4865404cf5afbb9f239a90f219
huntrreed <146265520+huntrreed@users.noreply.github.com>
2024-03-29 16:54:30 -0600

    worked on header component

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,46 +1,61 @@
 import { Link } from 'react-router-dom';
-
+import { Button, FloatButton } from 'antd';
 import Auth from '../../utils/auth';
+import { InfoCircleOutlined, DollarOutlined } from '@ant-design/icons';
 
 const Header = () => {
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
+
   return (
-    <header className="bg-info text-dark mb-4 py-3 display-flex align-center">
-      <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
-        <Link className="text-dark" to="/">
-          <h1 className="m-0" style={{ fontSize: '3rem' }}>
-            Tech Friends
-          </h1>
-        </Link>
-        <p className="m-0" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
-          Meet your new programming pals.
-        </p>
-        <div>
+    <>
+      <header className="header header-main">
+        <div className="header-content">
+          <Link to="/">
+            <h1 className="header-title m-0">TaskMaster</h1>
+          </Link>
+          <p className="header-description m-0">
+            Use your time better or something.
+          </p>
+        </div>
+
+        {/* Auth buttons */}
+        <div className="auth-buttons">
           {Auth.loggedIn() ? (
             <>
-              <Link className="btn btn-lg btn-primary m-2" to="/me">
-                View My Profile
+              <Link to="/me">
+                <Button type="primary" className="m-2">View My Profile</Button>
               </Link>
-              <button className="btn btn-lg btn-light m-2" onClick={logout}>
-                Logout
-              </button>
+              <Button className="m-2" onClick={logout}>Logout</Button>
             </>
           ) : (
             <>
-              <Link className="btn btn-lg btn-primary m-2" to="/login">
-                Login
+              <Link to="/login">
+                <Button type="primary" className="m-2">Login</Button>
               </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/signup">
-                Signup
+              <Link to="/signup">
+                <Button className="m-2">Signup</Button>
               </Link>
             </>
           )}
         </div>
+      </header>
+
+      {/* Floating buttons for About and Donate, these need some styling work */}
+        {/* Floating buttons */}
+        <div className="floating-buttons-container">
+        <FloatButton.Group>
+          <Link to="/about">
+            <FloatButton icon={<InfoCircleOutlined />} tooltip="About" />
+          </Link>
+          <Link to="/donate">
+            <FloatButton icon={<DollarOutlined />} tooltip="Donate" />
+          </Link>
+        </FloatButton.Group>
       </div>
-    </header>
+      </>
   );
 };
 
