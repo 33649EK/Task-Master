f72011cd6323cd0a16ee0e08bc8698420842bc7e
Evan Keller <33649ek@gmail.com>
2024-03-28 20:47:13 -0600

    changes a couple more 'skills' refrences

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -23,9 +23,9 @@ const typeDefs = `
     addProfile(name: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
 
-    addSkill(profileId: ID!, skill: String!): Profile
+    addTodo(profileId: ID!, todos: String!): Profile
     removeProfile: Profile
-    removeSkill(skill: String!): Profile
+    removeTodo(todos: String!): Profile
   }
 `;
 
