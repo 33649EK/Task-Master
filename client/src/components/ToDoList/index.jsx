78d3e3334f78c0f8e43d49f57591884f767660dc
Jadah Pierce <145732309+heyitsmejadah@users.noreply.github.com>
2024-03-31 14:59:20 -0600

    Added profile JSX with Queries, basic functionality of break screen

--- client/src/components/ToDoList/index.jsx
+++ client/src/components/ToDoList/index.jsx
@@ -1,31 +1,27 @@
 //none of this is done just put this in here for functionality while working on the timer thing 
 
 import React from 'react';
-import { List, Card } from 'antd';
+import { Card, Input } from 'antd';
 
 const ToDoList = () => {
-  // made up stuff
-  const tasks = [
-    { title: 'Task 1' },
-    { title: 'Task 2' },
-    { title: 'Task 3' },
-  ];
 
   return (
     <Card title="To Do">
-      <List
-        itemLayout="horizontal"
-        dataSource={tasks}
-        renderItem={item => (
-          <List.Item>
-            <List.Item.Meta
-              title={item.title}
-            />
-          </List.Item>
-        )}
-      />
+      <Input placeholder="Wash the Dishes" />
+      <br />
+      <Input></Input>
+      <br />
+      <Input></Input>
+      <br />
+      <Input></Input>
+      <br />
+      <Input></Input>
+      <br />
+      <Input></Input>
+      <br />
     </Card>
   );
 };
 
 export default ToDoList;
+
