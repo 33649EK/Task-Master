3ed68f634768f55e3524d8b64bb714c1c3b95cf4
huntrreed <146265520+huntrreed@users.noreply.github.com>
2024-04-01 15:38:35 -0600

    styling on the login and signup buttons, reworked these to be ant components

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,8 +2,13 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
+import { Form, Input, Button, Card, Typography } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
+const { Title } = Typography;
 
 import Auth from '../utils/auth';
+
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
@@ -40,55 +45,56 @@ const Login = (props) => {
     });
   };
 
+  
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-lg-10">
-        <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Login</h4>
-          <div className="card-body">
-            {data ? (
-              <p>
-                Success! You may now head{' '}
-                <Link to="/">back to the homepage.</Link>
-              </p>
-            ) : (
-              <form onSubmit={handleFormSubmit}>
-                <input
-                  className="form-input"
-                  placeholder="Your email"
-                  name="email"
-                  type="email"
-                  value={formState.email}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="******"
-                  name="password"
-                  type="password"
-                  value={formState.password}
-                  onChange={handleChange}
-                />
-                <button
-                  className="btn btn-block btn-info"
-                  style={{ cursor: 'pointer' }}
-                  type="submit"
-                >
-                  Submit
-                </button>
-              </form>
-            )}
-
-            {error && (
-              <div className="my-3 p-3 bg-danger text-white">
-                {error.message}
-              </div>
-            )}
+    <div className="login-page-container">
+      {data && data.login ? (
+        <p className="success-message">
+          Success! You may now head <Link to="/">back to the homepage.</Link>
+        </p>
+      ) : (
+        <Card className="login-card">
+          <div className="login-card-subheader">
+            Already have an account? Log in here!
           </div>
-        </div>
-      </div>
-    </main>
+          <Form
+            name="login"
+            initialValues={{ remember: true }}
+            onFinish={handleFormSubmit}
+            className="login-form"
+          >
+          <Form.Item
+            name="email"
+            rules={[{ required: true, message: 'Please input your email!' }]}
+          >
+            <Input
+              prefix={<UserOutlined />}
+              placeholder="Your email"
+              onChange={handleChange}
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password
+              prefix={<LockOutlined />}
+              placeholder="Password"
+              onChange={handleChange}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              Submit
+            </Button>
+          </Form.Item>
+          </Form>
+          {error && <div className="login-error">{error.message}</div>}
+        </Card>
+      )}
+    </div>
   );
 };
+
 
 export default Login;
